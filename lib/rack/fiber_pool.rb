d4f051b6cdc9ed83ce5def8cf833394a022117db
Peter Kieltyka <peter.kieltyka@nulayer.com>
2010-07-16 15:22:35 -0400

    Provide support for async applications

--- lib/rack/fiber_pool.rb
+++ lib/rack/fiber_pool.rb
@@ -1,21 +1,34 @@
 require 'fiber_pool'
 
 module Rack
+
+  # Run each request in a Fiber.  This FiberPool is provided by em_postgresql.
   class FiberPool
+    ASYNC_RESPONSE = [-1, {}, []].freeze
+    
     def initialize(app)
       @app = app
       @fiber_pool = ::FiberPool.new
       yield @fiber_pool if block_given?
     end
-
+    
     def call(env)
       call_app = lambda do
         result = @app.call(env)
-        env['async.callback'].call result
+        
+        # Check if it's an async app wrapped in a fiber
+        if result == ASYNC_RESPONSE
+          f = Fiber.current
+          env['async.close'].callback { f.resume }
+          Fiber.yield
+        else
+          env['async.callback'].call result
+        end
       end
       
       @fiber_pool.spawn(&call_app)
-      throw :async
+      
+      ASYNC_RESPONSE
     end
   end
 end
